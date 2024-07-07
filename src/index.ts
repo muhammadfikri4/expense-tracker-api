859b1f135a69225072ca0e8a334d7fe948a9ab15
muhammadfikri4 <unicornhorse1@gmail.com>
2024-07-07 12:33:54 +0700

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import { ENV } from './libs'
@@ -8,6 +9,13 @@ import routes from './routes'
 const app = express()
 const port = ENV.PORT || 8000
 dotenv.config()
+app.use(cors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'],
+    credentials: true,
+    preflightContinue: false
+}));
 app.use(cookieParser())
 app.use(express.json())
 app.use(bodyParser.json());
